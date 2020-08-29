e7f670c3e4cf77caf95030530da9a5b0b1ef4588
shashilsravan <shashilsravan.ss.ss@gmail.com>
2020-08-29 17:14:52 +0530

    Updates

---
+++ precache-manifest.23314d30b1752bb4f5689ce41273f271.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1a26d89c96904741cfde6f1fa7843e",
+    "url": "/rovae/index.html"
+  },
+  {
+    "revision": "fe4f065c0ae9295e0638",
+    "url": "/rovae/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "305b197ad0e0d62c79de",
+    "url": "/rovae/static/css/main.35782a11.chunk.css"
+  },
+  {
+    "revision": "fe4f065c0ae9295e0638",
+    "url": "/rovae/static/js/2.e109e555.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rovae/static/js/2.e109e555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "305b197ad0e0d62c79de",
+    "url": "/rovae/static/js/main.da3a26b9.chunk.js"
+  },
+  {
+    "revision": "0c52da42fac519a4f4b4",
+    "url": "/rovae/static/js/runtime-main.4a1ac055.js"
+  },
+  {
+    "revision": "dc99b7a312576e172d72242d64a823a2",
+    "url": "/rovae/static/media/Logo-01.dc99b7a3.svg"
+  },
+  {
+    "revision": "2de07da2f47451edc1dded639a44001c",
+    "url": "/rovae/static/media/logo.2de07da2.svg"
+  }
+]);
